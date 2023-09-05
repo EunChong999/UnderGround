e667d7dc9173f4421e963ad9735da26cd3204d33
김은총 <eunchonggim999@gmail.com>
2023-09-06 08:28:38 +0900

    FEAT : Adjustment to prevent direction change during gravity change

--- Assets/SwitchGravity.cs
+++ Assets/SwitchGravity.cs
@@ -8,6 +8,7 @@ public class SwitchGravity : MonoBehaviour
 
     private bool top;
     private bool facingRight = true;
+    private bool isChangingGravity = false;
 
     void Start()
     {
@@ -16,29 +17,41 @@ public class SwitchGravity : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.W))
+        ChangeGravity();
+    }
+
+    void ChangeGravity()
+    {
+        if (!isChangingGravity)
         {
-            Physics2D.gravity = new Vector2(0f, 10f);
-            //Rotation();
-            //Flip();
-        }
-        else if (Input.GetKeyDown(KeyCode.A))
-        {
-            Physics2D.gravity = new Vector2(-10f, 0f);
-            //Rotation();
-            //Flip();
-        }
-        else if (Input.GetKeyDown(KeyCode.S))
-        {
-            Physics2D.gravity = new Vector2(0f, -10f);
-            //Rotation();
-            //Flip();
-        }
-        else if (Input.GetKeyDown(KeyCode.D))
-        {
-            Physics2D.gravity = new Vector2(10f, 0f);
-            //Rotation();
-            //Flip();
+            if (Input.GetKeyDown(KeyCode.W))
+            {
+                Physics2D.gravity = new Vector2(0f, 10f);
+                rb.constraints = RigidbodyConstraints2D.FreezeRotation | RigidbodyConstraints2D.FreezePositionX;
+                //Rotation();
+                //Flip();
+            }
+            else if (Input.GetKeyDown(KeyCode.A))
+            {
+                Physics2D.gravity = new Vector2(-10f, 0f);
+                rb.constraints = RigidbodyConstraints2D.FreezeRotation | RigidbodyConstraints2D.FreezePositionY;
+                //Rotation();
+                //Flip();
+            }
+            else if (Input.GetKeyDown(KeyCode.S))
+            {
+                Physics2D.gravity = new Vector2(0f, -10f);
+                rb.constraints = RigidbodyConstraints2D.FreezeRotation | RigidbodyConstraints2D.FreezePositionX;
+                //Rotation();
+                //Flip();
+            }
+            else if (Input.GetKeyDown(KeyCode.D))
+            {
+                Physics2D.gravity = new Vector2(10f, 0f);
+                rb.constraints = RigidbodyConstraints2D.FreezeRotation | RigidbodyConstraints2D.FreezePositionY;
+                //Rotation();
+                //Flip();
+            }
         }
     }
 
@@ -62,6 +75,22 @@ public class SwitchGravity : MonoBehaviour
         Vector3 Scaler = transform.localScale;
         Scaler.x *= -1;
         transform.localScale = Scaler;
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Ground"))
+        {
+            isChangingGravity = false;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Ground"))
+        {
+            isChangingGravity = true;
+        }
     }
 }
 
