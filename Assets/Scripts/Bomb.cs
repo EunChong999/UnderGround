29bbe396a5c62566332fd9d45eb51d3ebd665373
김은총 <eunchonggim999@gmail.com>
2023-09-12 12:20:22 +0900

    FEAT : Create a bomb script

---
+++ Assets/Scripts/Bomb.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb : MonoBehaviour
+{
+    [SerializeField] float countdown;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        countdown -= Time.deltaTime;
+
+        if (countdown <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Scripts/BombSpawner.cs
+++ Assets/Scripts/BombSpawner.cs
@@ -5,18 +5,15 @@ using UnityEngine.Tilemaps;
 
 public class BombSpawner : MonoBehaviour
 {
-    [SerializeField] private Tilemap tilemap;
     [SerializeField] private GameObject bombPrefeb;
 
     private SwitchGravity switchGravity;
 
-    // Start is called before the first frame update
     void Start()
     {
         switchGravity = GetComponent<SwitchGravity>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space) && !switchGravity.isChangingGravity)
