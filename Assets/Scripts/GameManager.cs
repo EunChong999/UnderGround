db2c73a73b4f67ef467741c6f9dc47d596efed39
김은총 <eunchonggim999@gmail.com>
2023-09-14 16:26:57 +0900

    FEAT : Write a game rerun script

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,11 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public void ExitGame()
     {
         Application.Quit();
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
     }
 }
