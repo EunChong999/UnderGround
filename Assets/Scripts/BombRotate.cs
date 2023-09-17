46d4ad6b0dc0d18be5dd7a1a0aadb4a7cf7a483d
김은총 <eunchonggim999@gmail.com>
2023-09-17 19:59:02 +0900

    FEAT : Change rotation values according to version

--- Assets/Scripts/BombRotate.cs
+++ Assets/Scripts/BombRotate.cs
@@ -6,6 +6,10 @@ public class BombRotate : MonoBehaviour
 {
     void Update()
     {
+#if !UNITY_EDITOR
+        transform.Rotate(0, 0, 9);
+#else
         transform.Rotate(0, 0, 3);
+#endif
     }
 }
