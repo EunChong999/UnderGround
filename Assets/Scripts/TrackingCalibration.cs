e349b9670bf34c3b3728f524d22f577886a4ccde
EunChong999 <eunchonggim999@gmail.com>
2023-09-19 10:25:10 +0900

    FEAT : Adjust the enemy to rotate left and right in the direction the player is looking at

---
+++ Assets/Scripts/TrackingCalibration.cs
@@ -0,0 +1,21 @@
+using Pathfinding;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackingCalibration : MonoBehaviour
+{
+    public AIPath aIPath;
+
+    void Update()
+    {
+        if (aIPath.desiredVelocity.x >= 0.01f)
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+        }
+        else if(aIPath.desiredVelocity.x <= -0.01f)
+        {
+            transform.localScale = new Vector3(1f, 1f, 1f);
+        }
+    }
+}
