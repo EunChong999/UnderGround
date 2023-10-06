8c0c86f7b9451743e151cd6766253409c98f5b07
EunChong999 <eunchonggim999@gmail.com>
2023-10-07 02:41:25 +0900

    feat : 로딩씬 추가

--- Assets/Scripts/Enemies/SlimeMover.cs
+++ Assets/Scripts/Enemies/SlimeMover.cs
@@ -35,10 +35,6 @@ public class SlimeMover : MonoBehaviour
     private void Update()
     {
         CheckGroundOrWall();
-    }
-
-    private void FixedUpdate()
-    {
         Movement();
     }
 
