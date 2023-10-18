3e043f6573d7fcdfdbe23437230e7fdb6ccb01ba
EunChong999 <eunchonggim999@gmail.com>
2023-10-18 15:07:49 +0900

    feat : 슬라임 적 완성

--- Assets/SlimeTest.cs
+++ Assets/SlimeTest.cs
@@ -12,9 +12,11 @@ public class SlimeTest : MonoBehaviour
     [SerializeField]
     private bool wallDetected;
     [SerializeField]
-    private Transform groundCheckerPos;
+    private Transform groundPositionChecker;
     [SerializeField]
-    private Transform wallCheckerPos;
+    private Vector3[] groundPositionCheckerPos;
+    [SerializeField]
+    private Transform wallPositionChecker;
     [SerializeField]
     private float groundCheckDistance;
     [SerializeField]
@@ -23,46 +25,119 @@ public class SlimeTest : MonoBehaviour
     private LayerMask whatIsGround;
     [SerializeField]
     private bool hasTurn;
-    private float zAxisAdd;
     [SerializeField]
-    private int direction;
+    private float angle;
+    [SerializeField]
+    private Transform checkers;
+    [SerializeField]
+    private Vector2 groundCheckDir;
+    [SerializeField]
+    private Vector2 wallCheckDir;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         hasTurn = false;
+        checkers.parent = null;
     }
-
     private void Update()
     {
+        checkers.position = transform.position;
         CheckGroundOrWall();
+        ChangeAngle();
     }
-
     private void FixedUpdate()
     {
         Movement();
     }
 
+    void PosRound()
+    {
+        Vector3 position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+        float x = Mathf.Round(position.x);
+        float y = Mathf.Round(position.y);
+        float z = Mathf.Round(position.z);
+        transform.position = new Vector3(x, y, z);
+    }
+
     void CheckGroundOrWall()
     {
-        // 슬라임은 총 4방향으로 움직인다.
-        // 1.그라운드 체커 다운, 월 체커 라이트 <- 시작할 때 이 경우를 따른다.
-        // 2.그라운드 체커 라이트, 월 체커 업
-        // 3.그라운드 체커 업, 월 체커 레프트
-        // 4.그라운드 체커 레프트, 월 체커 다운
+        if (angle == 0 || angle == -360)
+        {
+            groundCheckDir = -transform.up;
+            wallCheckDir = transform.right;
+            groundPositionChecker.localPosition = groundPositionCheckerPos[0];
+        }
+        else if (angle == 270 || angle == -90)
+        {
+            groundCheckDir = -transform.up;
+            wallCheckDir = transform.right;
+            groundPositionChecker.localPosition = groundPositionCheckerPos[1];
+        }
+        else if (angle == 180 || angle == -180)
+        {
+            groundCheckDir = -transform.up;
+            wallCheckDir = transform.right;
+            groundPositionChecker.localPosition = groundPositionCheckerPos[2];
+        }
+        else if (angle == 90 || angle == -270)
+        {
+            groundCheckDir = -transform.up;
+            wallCheckDir = transform.right;
+            groundPositionChecker.localPosition = groundPositionCheckerPos[3];
+        }
 
-        // 예외로, 그라운드 체커와 월 체커 모두 false면,
-        // 그에 따른 조치를 취하여, 무작정 오른쪽으로만 이동한다.
+        groundDetected = Physics2D.Raycast(groundPositionChecker.position, groundCheckDir, groundCheckDistance, whatIsGround);
+        wallDetected = Physics2D.Raycast(wallPositionChecker.position, wallCheckDir, wallCheckDistance, whatIsGround);
+    }
+
+    void ChangeAngle()
+    {
+        if (groundDetected)
+        {
+            hasTurn = false;
+        }
+
+        if (angle <= -360)
+        {
+            angle = 0;
+        }
+
+        if (angle >= 360)
+        {
+            angle = 0;
+        }
+
+        if (!groundDetected)
+        {
+            if (!hasTurn)
+            {
+                PosRound();
+                angle -= 90;
+                transform.eulerAngles = new Vector3(0, 0, angle);
+                hasTurn = true;
+            }
+            else
+            {
+                if (!hasTurn)
+                {
+                    PosRound();
+                    angle += 90;
+                    transform.eulerAngles = new Vector3(0, 0, angle);
+                    hasTurn = true;
+                }
+            }
+        }
     }
 
     void Movement()
     {
-        rb.velocity = transform.right * speed;
+        rb.velocity = transform.right * speed * Time.fixedDeltaTime;
     }
 
     private void OnDrawGizmos()
     {
-        Gizmos.DrawLine(groundCheckerPos.position, new Vector2(groundCheckerPos.position.x, groundCheckerPos.position.y - groundCheckDistance));
-        Gizmos.DrawLine(wallCheckerPos.position, new Vector2(wallCheckerPos.position.x + wallCheckDistance, wallCheckerPos.position.y));
+        Gizmos.DrawLine(groundPositionChecker.position, new Vector2(groundPositionChecker.position.x, groundPositionChecker.position.y) + groundCheckDir * groundCheckDistance);
+        Gizmos.DrawLine(wallPositionChecker.position, new Vector2(wallPositionChecker.position.x, wallPositionChecker.position.y) + wallCheckDir * wallCheckDistance);
     }
 }
