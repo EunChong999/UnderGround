e8857a47f25b940d4c61790a2e4bee4ffc5b837b
EunChong999 <eunchonggim999@gmail.com>
2023-10-21 20:29:39 +0900

    fix : 시작 폭탄이 생성되지 않는 버그 수정

--- Assets/Scripts/Player/BombSpawner.cs
+++ Assets/Scripts/Player/BombSpawner.cs
@@ -25,14 +25,14 @@ public class BombSpawner : MonoBehaviour
         {
             if (!undergroundMovement.isMoving)
             {
-                if (Input.GetKey(KeyCode.UpArrow) && undergroundMovement.isSpaced[0])
+                if (Input.GetKey(KeyCode.UpArrow) && (undergroundMovement.isSpaced[0] || !undergroundMovement.isMoveStart))
                 {
                     animator.SetBool("isAttacking", true);
                     animator.SetFloat("horizontal", 0);
                     animator.SetFloat("vertical", 1);
                     bombInHand.SetActive(false);
                 }
-                else if (Input.GetKey(KeyCode.LeftArrow) && undergroundMovement.isSpaced[1])
+                else if (Input.GetKey(KeyCode.LeftArrow) && (undergroundMovement.isSpaced[1] || !undergroundMovement.isMoveStart))
                 {
                     animator.SetBool("isAttacking", true);
                     animator.SetFloat("horizontal", -1);
@@ -40,7 +40,7 @@ public class BombSpawner : MonoBehaviour
                     bombInHand.SetActive(true);
                     bombInHand.transform.localPosition = new Vector2(-0.15f, -0.25f);
                 }
-                else if (Input.GetKey(KeyCode.DownArrow) && undergroundMovement.isSpaced[2])
+                else if (Input.GetKey(KeyCode.DownArrow) && (undergroundMovement.isSpaced[2] || !undergroundMovement.isMoveStart))
                 {
                     animator.SetBool("isAttacking", true);
                     animator.SetFloat("horizontal", 0);
@@ -48,7 +48,7 @@ public class BombSpawner : MonoBehaviour
                     bombInHand.SetActive(true);
                     bombInHand.transform.localPosition = new Vector2(-0.15f, -0.25f);
                 }
-                else if (Input.GetKey(KeyCode.RightArrow) && undergroundMovement.isSpaced[3])
+                else if (Input.GetKey(KeyCode.RightArrow) && (undergroundMovement.isSpaced[3] || !undergroundMovement.isMoveStart))
                 {
                     animator.SetBool("isAttacking", true);
                     animator.SetFloat("horizontal", 1);
@@ -62,28 +62,28 @@ public class BombSpawner : MonoBehaviour
                     bombInHand.SetActive(false);
                 }
 
-                if (Input.GetKeyUp(KeyCode.UpArrow) && undergroundMovement.isSpaced[0])
+                if (Input.GetKeyUp(KeyCode.UpArrow) && (undergroundMovement.isSpaced[0] || !undergroundMovement.isMoveStart))
                 {
                     GameObject bomb = Instantiate(bombPrefeb, new Vector2(transform.position.x, transform.position.y + 0.25f), transform.rotation);
                     bomb.GetComponent<GridMovement>().x = 0;
                     bomb.GetComponent<GridMovement>().y = 1;
                     bomb.transform.eulerAngles = new Vector3(0, 0, 180);
                 }
-                else if (Input.GetKeyUp(KeyCode.LeftArrow) && undergroundMovement.isSpaced[1])
+                else if (Input.GetKeyUp(KeyCode.LeftArrow) && (undergroundMovement.isSpaced[1] || !undergroundMovement.isMoveStart))
                 {
                     GameObject bomb = Instantiate(bombPrefeb, new Vector2(transform.position.x - 0.25f, transform.position.y), transform.rotation);
                     bomb.GetComponent<GridMovement>().x = -1;
                     bomb.GetComponent<GridMovement>().y = 0;
                     bomb.transform.eulerAngles = new Vector3(0, 0, 270);
                 }
-                else if (Input.GetKeyUp(KeyCode.DownArrow) && undergroundMovement.isSpaced[2])
+                else if (Input.GetKeyUp(KeyCode.DownArrow) && (undergroundMovement.isSpaced[2] || !undergroundMovement.isMoveStart))
                 {
                     GameObject bomb = Instantiate(bombPrefeb, new Vector2(transform.position.x, transform.position.y - 0.25f), transform.rotation);
                     bomb.GetComponent<GridMovement>().x = 0;
                     bomb.GetComponent<GridMovement>().y = -1;
                     bomb.transform.eulerAngles = new Vector3(0, 0, 0);
                 }
-                else if (Input.GetKeyUp(KeyCode.RightArrow) && undergroundMovement.isSpaced[3])
+                else if (Input.GetKeyUp(KeyCode.RightArrow) && (undergroundMovement.isSpaced[3] || !undergroundMovement.isMoveStart))
                 {
                     GameObject bomb = Instantiate(bombPrefeb, new Vector2(transform.position.x + 0.25f, transform.position.y), transform.rotation);
                     bomb.GetComponent<GridMovement>().x = 1;
