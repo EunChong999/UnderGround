8258be1c950752c9d63bf142e90938b14fc5b914
EunChong999 <eunchonggim999@gmail.com>
2023-10-23 14:54:51 +0900

    feat : 페이드 인 페이드 아웃 효과 구현

---
+++ Assets/LevelChanger.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelChanger : MonoBehaviour
+{
+    private Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            FadeToLevel(1);
+        }
+    }
+
+    public void FadeToLevel(int levelIndex)
+    {
+        animator.SetTrigger("FadeOut");
+    }
+}
