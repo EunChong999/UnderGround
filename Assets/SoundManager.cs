4e33d2ae8ce31263de0cfc3cd1b33f51bce68826
EunChong999 <eunchonggim999@gmail.com>
2023-10-27 17:48:30 +0900

    feat : 사운드 매니저 추가

---
+++ Assets/SoundManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SoundManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject regular;
+    [SerializeField]
+    private GameObject battle;
+
+    void Update()
+    {
+        if(SceneManager.GetActiveScene().name == "Game Play Scene" || 
+           SceneManager.GetActiveScene().name == "Game Tutorial Scene")
+        {
+            regular.SetActive(false);
+            battle.SetActive(true);
+        }
+        else
+        {
+            regular.SetActive(true);
+            battle.SetActive(false);
+        }
+    }
+}
