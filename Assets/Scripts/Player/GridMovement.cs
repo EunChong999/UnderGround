d1c99be8b8594c39eb2d2c3e00b214ef09592dd1
EunChong999 <eunchonggim999@gmail.com>
2024-04-26 20:54:44 +0900

    feat : 그리드 이동 수정

--- Assets/Scripts/Player/GridMovement.cs
+++ Assets/Scripts/Player/GridMovement.cs
@@ -10,17 +10,12 @@ public class GridMovement : MonoBehaviour
     public LayerMask whatStopMovement;
     [HideInInspector] public float x;
     [HideInInspector] public float y;
-    [SerializeField] float delayTime;
-    WaitForSeconds waitForSeconds;
     UndergroundMovement undergroundMovement;
-    bool canMove;
     float curXPos;
     float curYPos;
 
     void Start()
     {
-        waitForSeconds = new WaitForSeconds(delayTime);
-        canMove = true;
         movePoint.parent = null;
         undergroundMovement = GetComponent<UndergroundMovement>();
     }
@@ -49,18 +44,14 @@ public class GridMovement : MonoBehaviour
 
         if (isMoveType)
         {
-            if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(x, 0, 0), .25f, whatStopMovement) && canMove && x != 0)
+            if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(x, 0, 0), .25f, whatStopMovement) && x != 0)
             {
                 movePoint.position += new Vector3(x, 0, 0);
-                StartCoroutine(DelayMove());
-                canMove = false;
             }
 
-            if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(0, y, 0), .25f, whatStopMovement) && canMove && y != 0)
+            if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(0, y, 0), .25f, whatStopMovement) && y != 0)
             {
                 movePoint.position += new Vector3(0, y, 0);
-                StartCoroutine(DelayMove());
-                canMove = false;
             }
         }
         else
@@ -75,12 +66,6 @@ public class GridMovement : MonoBehaviour
                 movePoint.position += new Vector3(0, curYPos, 0);
             }
         }
-    }
-
-    IEnumerator DelayMove()
-    {
-        yield return waitForSeconds;
-        canMove = true;
     }
 
     private void OnDrawGizmos()
